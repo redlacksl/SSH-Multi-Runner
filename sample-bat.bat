3444fd60c4a6fb505cedfea9e39d51afe078058f
redlacksl <redlacksl@gmail.com>
2016-08-03 23:42:35 -0600

    Revert "Sample batch file"
    This reverts commit 3669d8ac40211ac0dc8106974e22b5ff519768a7.

--- sample-bat.bat
+++
@@ -1,5 +0,0 @@
-set PYTHONHOME=C:\Users\me\AppData\Local\Continuum\Anaconda3
-set PYTHONPATH=C:\Users\me\AppData\Local\Continuum\Anaconda3\Lib
-set PATH=%PYTHONHOME%;%PATH%
-
-python C:\Users\me\Documents\GitHub\SSH-Multi-Runner\SSH-multi-runner.py C:\Users\me\Documents\GitHub\SSH-Multi-Runner\sample-script.txt myname
